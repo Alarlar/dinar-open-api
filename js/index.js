d7db98d7a8d85f82bf36e9060285364fa538d173
Alarlar <52812499+Alarlar@users.noreply.github.com>
2024-09-15 18:22:59 -0700

    second data point

--- js/index.js
+++ js/index.js
@@ -1,34 +1,41 @@
-// Creating my fetch
-
-const gitHubUsername = "Alarlar";
-
-fetch(`https://api.github.com/users/${gitHubUsername}/repos`)
-  .then((response) => {
-    return response.json(); // Converting response to JSON
-  })
-  .then((repositories) => {
-    console.log(repositories); // List of repositories
-
-    displayRepositories(repositories);
-  })
-  .catch((error) => {
-    console.error("Error fetching GitHub repositories:", error);
-    const projectSection = document.querySelector("#projects");
-    projectSection.innerHTML =
-      "<p>Could not load projects. Please try again later.</p>";
+document
+  .getElementById("artwork-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchArtwork();
   });
 
-// This function will display repo in list
+document
+  .getElementById("artist-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchArtist();
+  });
 
-function displayRepositories(repositories) {
-  const projectSection = document.querySelector("#projects");
-  const projectList = projectSection.querySelector("ul");
+function fetchArtwork() {
+  fetch("https://api.artic.edu/api/v1/artworks")
+    .then((response) => response.json())
+    .then((data) => {
+      const artwork = data.data[0];
+      const imageUrl = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`;
+      document.getElementById("content").innerHTML = `
+              <h2>${artwork.title}</h2>
+              <p>Artist: ${artwork.artist_title}</p>
+              <img src="${imageUrl}" alt="${artwork.title}" style="max-width: 100%; height: auto;">
+          `;
+    })
+    .catch((error) => console.error("Error fetching artwork:", error));
+}
 
-  for (let i = 0; i < repositories.length; i++) {
-    const repo = repositories[i];
-    const project = document.createElement("li");
-
-    project.innerText = repo.name;
-    projectList.appendChild(project);
-  }
+function fetchArtist() {
+  fetch("https://api.artic.edu/api/v1/artists")
+    .then((response) => response.json())
+    .then((data) => {
+      const artist = data.data[0];
+      document.getElementById("content").innerHTML = `
+              <h2>${artist.title}</h2>
+              <p>Birth Date: ${artist.birth_date}</p>
+          `;
+    })
+    .catch((error) => console.error("Error fetching artist:", error));
 }
